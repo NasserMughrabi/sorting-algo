447c42ecb1d3108a7223c3c4dc0100afb826bd7d
Nasser Mughrabi <abdalnasseralmughrabi@gmail.com>
2022-07-15 09:13:43 -0600

    fixed bars height bug

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
   const [arrSize, setArrSize] = useState(25);
   // generate an arr of random numbers between 1 and 69 (1 and 69 included)
-  const arr = Array(arrSize).fill(5).map(() => Math.round(Math.random() * (69 - 1 + 1) + 1));
+  const arr = Array(arrSize).fill(5).map(() => Math.round(Math.random() * (65 - 1 + 1) + 1));
 
   return (
     <>
